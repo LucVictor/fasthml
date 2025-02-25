941d338238494fc56840f89c6c39d2d259769e9d
Lucas Victor <47704132+LucVictor@users.noreply.github.com>
2025-02-25 16:21:15 -0300

    update

--- formulario.py
+++ formulario.py
@@ -1,14 +1,14 @@
 from fasthtml.common import *
 
 def formularioObs(cliente, pessoas):
-    return Div(Form(method="post", action=f"/cadastrarobs", id='tela', hx_post=f'/cadastrarobs')(
+    return Div(Form(method="post", action=f"/cadastrarobs/", hx_post='/cadastrarobs/')(
         Fieldset(
-            Label('Cod. Cliente', Input(type="text", value=f'{cliente.codigo}'), id='codigoCliente'),
-            Label('Cliente', Input(type="text", value=f'{cliente.nome}'), id='cliente'),
-            Label("Motorista", *[Select(Option(pessoa.nome), value=f'{pessoa.nome}') for pessoa in pessoas],id='motorista'),
-            Label("Solicitante", *[Select(Option(pessoa.nome), value=f'{pessoa.nome}') for pessoa in pessoas], id='solicitante'),
-            Label("Picotes preso?", Select(Option('Sim'), Option('Não'), id='picote')),
-            Label('Data da ocorrência', Input(type='date'), id='data')
+            Label('Cod. Cliente', Input(type="text", value=f'{cliente.codigo}', name='codigoCliente')),
+            Label('Cliente', Input(type="text", value=f'{cliente.nome}'), name='cliente'),
+            Label("Motorista", *[Select(Option(pessoa.nome), value=f'{pessoa.nome}', name='motorista' ) for pessoa in pessoas]),
+            Label("Solicitante", *[Select(Option(pessoa.nome), value=f'{pessoa.nome}', name='solicitante') for pessoa in pessoas]),
+            Label("Picotes preso?", Select(Option('Sim'), Option('Não'), name='picote')),
+            Label('Data da ocorrência', Input(type='date', name='data'))
 
         ),
         Button("Criar", type="submit"), style='margin: 0 auto; text-align: center;'
