2400818d0f655837848d19562480ee7ba9ea51d8
LucVictor <lucasvictor999@Outlook.com.br>
2025-03-07 16:09:38 -0300

    update

--- layout.py
+++ layout.py
@@ -3,5 +3,5 @@ from topbar import topbar
 
 def layout(conteudo=''):
     if conteudo:
-        return Div(Div(topbar(), cls="no-print"), Div(conteudo, id='conteudo', style='width: 90%; margin: 0 auto;'), style='', id='layout')
+        return Div(Div(topbar(), cls="no-print"), Div(conteudo, id='conteudo', style='width: 90%; margin: 0 auto;'),  style='', id='layout')
     return Div(Div(topbar()), Div(id='conteudo'))
